556bce29d81f323a8a83a49cec9803a4f549fe63
DanielGad <adeyemidanielgad@gmail.com>
2024-02-01 20:50:50 +0100

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import "@/assets/header.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Header = () => {
@@ -9,42 +9,59 @@ const Header = () => {
     setActiveMenu(menu);
   };
 
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Update the state based on the scroll position
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="header-container">
+    <div className={`header-container ${isScrolled ? 'opaque' : 'transparent'} `}>
       <div style={{width: "60px"}}>
         <img src="/G.png" alt="Gad logo" width={"100%"}/>
       </div>
 
-      <div className="menus">
-        <span className={activeMenu === 'home' ? 'active' : ''}>
+      <div className={`menus `}>
+        <div className={activeMenu === 'home' ? 'active' : ''}>
           <Link to="home" spy={true} smooth={true} onClick={() => handleMenuClick('home')}>
             HOME
           </Link>
-        </span>
+        </div>
 
-        <span className={activeMenu === 'about' ? 'active' : ''}>
+        <div className={activeMenu === 'about' ? 'active' : ''}>
           <Link to="about" spy={true} smooth={true} onClick={() => handleMenuClick('about')}>
             ABOUT
           </Link>
-        </span>
+        </div>
 
-        <span className={activeMenu === 'contact' ? 'active' : ''}>
+        <div className={activeMenu === 'contact' ? 'active' : ''}>
           <Link to="contact" spy={true} smooth={true} onClick={() => handleMenuClick('contact')}>
             CONTACT
           </Link>
-        </span>
+        </div>
 
-        <span className={activeMenu === 'skills' ? 'active' : ''}>
+        <div className={activeMenu === 'skills' ? 'active' : ''}>
           <Link to="skills" spy={true} smooth={true} onClick={() => handleMenuClick('skills')}>
             SKILLS
           </Link>
-        </span>
+        </div>
 
-        <span className={activeMenu === 'blogs' ? 'active' : ''}>
+        <div className={activeMenu === 'blogs' ? 'active' : ''}>
           <Link to="blogs" spy={true} smooth={true} onClick={() => handleMenuClick('blogs')}>
             BLOGS
           </Link>
-        </span>
+        </div>
     
       </div>
       <button className="call">Call To Action</button>
