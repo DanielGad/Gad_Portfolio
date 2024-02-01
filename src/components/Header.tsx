9c97202eee511b4d34b72f8202acc163d69b4302
DanielGad <adeyemidanielgad@gmail.com>
2024-02-01 18:12:18 +0100

    header update

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import "@/assets/header.css";
+import { useState } from 'react';
+import { Link } from 'react-scroll';
+
+const Header = () => {
+  const [activeMenu, setActiveMenu] = useState("");
+
+  const handleMenuClick = (menu: string) => {
+    setActiveMenu(menu);
+  };
+
+  return (
+    <div className="header-container">
+      <div style={{width: "60px"}}>
+        <img src="/G.png" alt="Gad logo" width={"100%"}/>
+      </div>
+
+      <div className="menus">
+        <span className={activeMenu === 'home' ? 'active' : ''}>
+          <Link to="home" spy={true} smooth={true} onClick={() => handleMenuClick('home')}>
+            HOME
+          </Link>
+        </span>
+
+        <span className={activeMenu === 'about' ? 'active' : ''}>
+          <Link to="about" spy={true} smooth={true} onClick={() => handleMenuClick('about')}>
+            ABOUT
+          </Link>
+        </span>
+
+        <span className={activeMenu === 'contact' ? 'active' : ''}>
+          <Link to="contact" spy={true} smooth={true} onClick={() => handleMenuClick('contact')}>
+            CONTACT
+          </Link>
+        </span>
+
+        <span className={activeMenu === 'skills' ? 'active' : ''}>
+          <Link to="skills" spy={true} smooth={true} onClick={() => handleMenuClick('skills')}>
+            SKILLS
+          </Link>
+        </span>
+
+        <span className={activeMenu === 'blogs' ? 'active' : ''}>
+          <Link to="blogs" spy={true} smooth={true} onClick={() => handleMenuClick('blogs')}>
+            BLOGS
+          </Link>
+        </span>
+        
+        <span>Call To Action</span>
+      </div>
+    </div>
+  )
+}
+
+export default Header
